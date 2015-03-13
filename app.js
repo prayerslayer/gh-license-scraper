f8e7adf657c3cb624cbc0025c308fd1a4d4acd67
Nikolaus Piccolotto <prayerslayer@gmail.com>
2015-03-13 23:48:20 +0100

    should work

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var superagent = require( 'superagent' ),
     _ = require( 'lodash' ),
     BASE_URL = 'https://api.github.com',
     TOKEN = args.token,
-    FILENAME = args.filename || 'repos.csv',
+    FILENAME = args.out || 'repos.csv',
     fs = require( 'fs' ),
     queue = [],
     stop = false,
@@ -11,7 +11,7 @@ var superagent = require( 'superagent' ),
 
 function jsonToCsv( repos ) {
     return repos.reduce( function( str, repo ) {
-        return str += repo.id + ';' + repo.name + ';' + repo.license + ';\n';
+        return str += repo.id + ';' + repo.name + ';' + repo.stars + ';' + repo.watchers + ';' + repo.license + ';\n';
     }, '');
 }
 
@@ -21,13 +21,40 @@ function appendToFile( repos ) {
 }
 
 function save() {
+    if ( stop ) {
+        return;
+    }
     stop = true;
     console.log( 'last repo was', lastRepo );
     appendToFile( queue );
 }
 
+function fetchSingle( repo ) {
+    console.log( 'Fetching', repo.full_name );
+    superagent
+        .get( BASE_URL + '/repos/' + repo.full_name )
+        .set( 'Authorization', 'Token ' + TOKEN )
+        .set( 'Accept', 'application/vnd.github.drax-preview+json' ) // otherwise no license info
+        .end( function( err, res ) {
+            if ( err ) {
+                console.log( 'retrying', repo.full_name );
+                return stop ? false : fetchSingle( repo );
+            }
+            var full = JSON.parse( res.text );
+            console.log( 'Got', full.full_name );
+            queue.push({
+                id: full.id,
+                name: full.full_name,
+                license : full.license ? full.license.key : false,
+                stars: full.stargazers_count,
+                watchers: full.watchers_count,
+                language: full.language
+            });
+        });
+}
+
 function requestNext() {
-    console.log( 'Requesting...' );
+    console.log( 'Requesting batch...' );
     if ( stop ) {
         return;
     }
@@ -35,25 +62,18 @@ function requestNext() {
         .get( BASE_URL + '/repositories' )
         .query({ since: lastRepo || null })
         .set( 'Authorization', 'Token ' + TOKEN )
-        .set( 'Accept', 'application/vnd.github.drax-preview+json' ) // otherwise no license info
         .end( function( err, res ) {
             if ( err ) {
                 console.error( err );
                 return;
             }
-            var repos = JSON.parse( res.text ).map( function( repo ) {
-                return {
-                    id: repo.id,
-                    name: repo.full_name,
-                    license: repo.license ? repo.license.key : false
-                };
-            });
-            console.log( repos );
+            var repos = JSON.parse( res.text );
+            
             lastRepo = _.last( repos ).id;
-            queue.push.apply( queue, repos );
 
-            console.log( 'Now got', queue.length, 'repos' );
-            setTimeout( requestNext, 1000 );
+            repos.forEach( fetchSingle );
+            
+            setTimeout( requestNext, 100000 );
         });
 }
 
